e56eec2571ecfc1988e2fc8b4001b5241a16ea62
M.Reza <40055314+mm-reza@users.noreply.github.com>
2022-11-19 15:05:48 -0600

    Update views.py

--- teams/views.py
+++ teams/views.py
@@ -241,11 +241,8 @@ def QuarterFinal(request):
     for q in a:
         q.append(q)
 
-    a_new = []
-    
-    
-   
-        print('---------', a, a_new)
+    a_new = a-q
+    print('---------', a, a_new)
     
             
     
