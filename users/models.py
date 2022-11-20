16f9c6f2f5ee7fc6e9fc94f9d34d14509092c1b9
M.Reza <40055314+mm-reza@users.noreply.github.com>
2022-11-20 06:24:10 -0600

    v

--- users/models.py
+++ users/models.py
@@ -7,7 +7,7 @@ from PIL import Image
 
 class Profile(models.Model):
 	user = models.OneToOneField(User, on_delete=models.CASCADE)
-	# image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+	image = models.ImageField(default='default.jpg', upload_to='profile_pics')
 	full_name = models.CharField(blank=True, max_length=50)
 	points = models.CharField(blank=True, max_length=50, default="0")
 
@@ -15,12 +15,12 @@ class Profile(models.Model):
 		return f'{self.user.username} Profile' 
 
 
-	# def save(self, ** kwargs):
-	# 	super().save()
+	def save(self, ** kwargs):
+		super().save()
 
-		# img = Image.open(self.image.path)
+		img = Image.open(self.image.path)
 
-		# if img.height > 300 or img.width > 300:
-		# 	output_size = (300, 300)
-		# 	img.thumbnail(output_size)
-		# 	img.save(self.image.path)	
+		if img.height > 300 or img.width > 300:
+			output_size = (300, 300)
+			img.thumbnail(output_size)
+			img.save(self.image.path)	
