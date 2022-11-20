74d9c8cf8c89cf3bc6f08fea324be7ef051f2673
M.Reza <40055314+mm-reza@users.noreply.github.com>
2022-11-20 03:58:27 -0600

    Update admin.py

--- teams/admin.py
+++ teams/admin.py
@@ -5,10 +5,10 @@ from .models import Teams, Predictions
 # Register your models here.
 
 class TeamAdmin(admin.ModelAdmin):
-    list_display = ['user', 'round', 'team', 'ranking', 'group', 'point']
+    list_display = ['user', 'index', 'round', 'team', 'ranking', 'group', 'point']
 
 class PredictAdmin(admin.ModelAdmin):
-    list_display = ['user', 'round', 'rank', 'group', 'prediction', 'result', 'point']
+    list_display = ['user', 'index', 'round', 'rank', 'group', 'prediction', 'result', 'point']
 
 admin.site.register(Teams, TeamAdmin)
 admin.site.register(Predictions, PredictAdmin)
