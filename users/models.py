91cb380eaf793c17bc80f987e5200e6aa495dc64
M.Reza <40055314+mm-reza@users.noreply.github.com>
2022-11-20 10:30:43 -0600

    Update models.py

--- users/models.py
+++ users/models.py
@@ -7,7 +7,7 @@ from PIL import Image
 
 class Profile(models.Model):
 	user = models.OneToOneField(User, on_delete=models.CASCADE)
-	image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+	#image = models.ImageField(default='default.jpg', upload_to='profile_pics')
 	full_name = models.CharField(blank=True, max_length=50)
 	points = models.CharField(blank=True, max_length=50, default="0")
 
@@ -17,10 +17,3 @@ class Profile(models.Model):
 
 	def save(self, ** kwargs):
 		super().save()
-
-		img = Image.open(self.image.path)
-
-		if img.height > 300 or img.width > 300:
-			output_size = (300, 300)
-			img.thumbnail(output_size)
-			img.save(self.image.path)
